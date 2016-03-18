d770697f82c0acd798a01439c2502e6f998672c6
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-03-18 22:23:25 +0100

    custom tab view in view pager

--- app/src/main/java/com/kitowcy/t_range/MainActivity.java
+++ app/src/main/java/com/kitowcy/t_range/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kitowcy.t_range;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +12,13 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static String POSITION = "POSITION";
 
     @Bind(R.id.main_view_pager)
     ViewPager mainViewPager;
+
+    @Bind(R.id.slide_tab)
+    TabLayout slideTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,28 @@ public class MainActivity extends AppCompatActivity {
         prepareViewPager();
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(POSITION, mainViewPager.getCurrentItem());
+        Log.d(TAG, "onSaveInstanceState: saved pager item: " + mainViewPager.getCurrentItem());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mainViewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
+        Log.d(TAG, "onRestoreInstanceState: saved pager: " + savedInstanceState.getInt(POSITION));
+    }
+
     private void prepareViewPager() {
         Log.d(TAG, "prepareViewPager()");
 
-        MainViewPagerAdapter mvpa = new MainViewPagerAdapter(getSupportFragmentManager());
+        MainViewPagerAdapter mvpa = new MainViewPagerAdapter(getSupportFragmentManager(), this);
         mainViewPager.setAdapter(mvpa);
+        slideTab.setupWithViewPager(mainViewPager);
+//        for (int x = 0; x < slideTab.getTabCount(); x++) {
+//            slideTab.getTabAt(x).setIcon(R.drawable.signal_icon);
+//        }
     }
 }
