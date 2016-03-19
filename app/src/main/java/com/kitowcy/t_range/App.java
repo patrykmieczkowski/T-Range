eddeff1d1b91b364f670daf769b2a1f3ef50e3a6
VaccumMan <lukmar93@o2.pl>
2016-03-19 10:51:20 +0100

    Merge branch 'master' of https://github.com/patrykmieczkowski/T-Range
    Conflicts:
    	app/app.iml

--- app/src/main/java/com/kitowcy/t_range/App.java
+++ app/src/main/java/com/kitowcy/t_range/App.java
@@ -2,15 +2,14 @@ package com.kitowcy.t_range;
 
 import android.app.Application;
 import android.location.Location;
-import android.location.LocationManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 
-import java.util.concurrent.TimeUnit;
+import com.google.android.gms.location.LocationRequest;
 
-import ru.solodovnikov.rxlocationmanager.LocationRequestBuilder;
-import ru.solodovnikov.rxlocationmanager.LocationTime;
+import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
 import rx.Subscriber;
+import rx.Subscription;
 
 /**
  * Created by Łukasz Marczak
@@ -21,11 +20,43 @@ public class App extends Application {
     public static final String TAG = App.class.getSimpleName();
     public static App INSTANCE;
     public Location location;
+    public Subscription subscription;
 
     @Override
     public void onCreate() {
         super.onCreate();
         INSTANCE = this;
+
+        LocationRequest request = LocationRequest.create() //standard GMS LocationRequest
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setNumUpdates(5)
+                .setInterval(100);
+
+        ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(this);
+          subscription = locationProvider.getUpdatedLocation(request)
+                .subscribe(new Subscriber<Location>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i(TAG, "onCompleted: ");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: ", e);
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext(Location location) {
+                        App.this.location = location;
+                        final String name = Thread.currentThread().getName();
+                        Log.d(TAG, "name: " + name + ", location: " +
+                                (location != null ? (location.getLatitude() + "," + location.getLongitude())
+                                        : "Location is empty =("));
+                    }
+                });
+
+
         startLocationUpdates();
     }
 
@@ -34,31 +65,6 @@ public class App extends Application {
 
     public void startLocationUpdates() {
         Log.d(TAG, "startLocationUpdates: ");
-        final LocationRequestBuilder locationRequestBuilder = new LocationRequestBuilder(this);
 
-        locationRequestBuilder.addLastLocation(LocationManager.NETWORK_PROVIDER, new LocationTime(30, TimeUnit.SECONDS), false)
-                .addRequestLocation(LocationManager.GPS_PROVIDER, new LocationTime(10, TimeUnit.SECONDS))
-                .setDefaultLocation(new Location(LocationManager.PASSIVE_PROVIDER))
-                .create().subscribe(new Subscriber<Location>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, e.toString());
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onNext(Location location) {
-                Log.d(TAG, "onNext: ");
-                App.this.location = location;
-                final String name = Thread.currentThread().getName();
-                Log.d(TAG, "name: "+name + ", location: " +
-                        (location != null ? location.toString() : "Location is empty =("));
-            }
-        });
     }
 }
